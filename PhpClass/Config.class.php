17f774aea57ff50bc5e135c6f896088274117ae7
gowthamaravi91 <gowthamrdx@gmail.com>
2014-05-08 10:38:08 +0530

    basic php files

---
+++ PhpClass/Config.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gowthamaravi
+ * Date: 2/24/14
+ * Time: 6:18 AM
+ */
+
+
+
+
+class Config {
+
+    const HOST_NAME = "localhost";
+
+    const DATABASE_NAME ="dbthine";
+
+    const USER_NAME = "root";
+
+    const PASSWORD = "";
+
+//    const HOST_NAME = "dbthine.db.10761716.hostedresource.com";
+//
+//    const DATABASE_NAME ="dbthine";
+//
+//    const USER_NAME = "dbthine";
+//
+//    const PASSWORD = "ThIndgd435#";
+
+
+} 
